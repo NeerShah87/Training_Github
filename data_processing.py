e9a0416b7165c75cd576d91ae8e0037e8e785537
Neershah87 <neeraj.bittu87@gmail.com>
2021-11-30 00:22:14 +0530

    reset code

--- data_processing.py
+++ data_processing.py
@@ -1,24 +1,3 @@
 
 print("add all fraud detection code related to fraud detection")
 print("modified codes")
-
-a = 5
-b = 7
-
-def sum(a,b):
-    c = a + b
-    return c
-
-print ("Sum function")
-
-def multiply(a,b):
-    d = a*b
-    return d
-
-print ("multiply function")
-
-def division(a,b):
-    e = a/b
-    return b
-
-print("division function")
