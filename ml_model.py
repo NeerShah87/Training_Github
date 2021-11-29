e70c709f2dbc5c467b9884a578e564e3b6652540
Neershah87 <neeraj.bittu87@gmail.com>
2021-11-30 00:26:29 +0530

    added 3 ML models

--- ml_model.py
+++ ml_model.py
@@ -1,2 +1,14 @@
 
 print("all ml model codes")
+
+
+def model1():
+    print("added code for model 1")
+
+def model2():
+    print("added code for model 2")
+
+def model3():
+    print("added code for model 3")
+
+    
