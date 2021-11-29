8f9e6271300f9ef68b4bffba63942ea9d0d6c01d
Neershah87 <neeraj.bittu87@gmail.com>
2021-11-29 23:23:16 +0530

    Added a new function sum

--- data_processing.py
+++ data_processing.py
@@ -1,3 +1,12 @@
 
 print("add all fraud detection code related to fraud detection")
 print("modified codes")
+
+a = 5
+b = 7
+
+def sum(a,b):
+    c = a + b
+    return a
+
+print ("added a new function which is not correct")
